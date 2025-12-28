832db942ccc993e565ac95a1a676c4d7bcb2d361
srinijammula <srinijammula5@gmail.com>
2025-12-28 12:54:37 -0500

    cleanup auth and constants logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { clearUser, setUser } from "../utils/userSlice";
+import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
     const dispatch = useDispatch();
@@ -18,7 +19,7 @@ const Header = () => {
         });
     }
     useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribe=onAuthStateChanged(auth, (user) => {
           if (user) {
             // User is signed in
             const {uid, email, displayName, photoURL} = user;
@@ -30,11 +31,12 @@ const Header = () => {
             navigate('/');
           }
         });
+        return ()=> unsubscribe();
       },[]);
     return(
         <div className="absolute p-4 w-screen bg-gradient-to-b from-black z-10 flex justify-between">
             <img className="w-44"
-            src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
+            src={LOGO_URL} alt="logo" />
             {user && <div className="flex items-center gap-4">
                 <img className="w-10 h-10"
                 src={user?.photoURL} alt="usericon"/>
